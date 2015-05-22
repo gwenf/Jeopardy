4628d5299160df61f58221beb8c4effca0cfe451
T.B. Stan <gorillaz28@users.noreply.github.com>
2015-05-22 11:56:34 -0400

    Added jQuery file to repo branch.

---
+++ jeoquery.js
@@ -0,0 +1,11 @@
+//jQuery "main"
+$(document).ready(function(){
+	$
+})
+
+/* function to disable the selected Jeopardy question
+ * @param quest contains the clicked element.
+*/
+function DisableQuestion(quest){
+	
+}
